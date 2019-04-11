81f9c72bf78e2fd06c0d733840507a4f576fa6ca
Kornel Dubieniecki <lekterable@gmail.com>
2019-04-12 01:00:13 +0200

    Make cells editable

--- src/table-csv/components/Cell.js
+++ src/table-csv/components/Cell.js
@@ -1,5 +1,39 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 export default ({ header, value }) => {
-  return header ? <th>{value}</th> : <td>{value}</td>
+  const [content, setContent] = useState(value)
+  const [editMode, setEditMode] = useState(false)
+  const toggleMode = () => setEditMode(mode => !mode)
+  const handleFocus = ref => ref && ref.focus()
+  const handleChange = e => setContent(e.target.value)
+
+  return header ? (
+    <th>
+      {editMode ? (
+        <input
+          type="text"
+          value={content}
+          onBlur={toggleMode}
+          onChange={handleChange}
+          ref={handleFocus}
+        />
+      ) : (
+        <div onClick={toggleMode}>{content}</div>
+      )}
+    </th>
+  ) : (
+    <td>
+      {editMode ? (
+        <input
+          type="text"
+          value={content}
+          onBlur={toggleMode}
+          onChange={handleChange}
+          ref={handleFocus}
+        />
+      ) : (
+        <div onClick={toggleMode}>{content}</div>
+      )}
+    </td>
+  )
 }
