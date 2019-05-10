5418fd003feeb16e747c451ee7b9913bb7cb66c8
Kornel Dubieniecki <lekterable@gmail.com>
2019-05-10 23:31:52 +0200

    Use constants in reducer

--- src/table-csv/reducers/index.js
+++ src/table-csv/reducers/index.js
@@ -1,13 +1,20 @@
 import uuid from 'uuid'
+import {
+  ADD_COLUMN,
+  ADD_ROW,
+  REMOVE_COLUMN,
+  REMOVE_ROW,
+  UPDATE_CONTENT
+} from '../actions'
 
 export default (state, action) => {
   switch (action.type) {
-    case 'UPDATE_CONTENT': {
+    case UPDATE_CONTENT: {
       const newContent = [...state.content]
       newContent[action.rowIndex][action.colIndex] = action.content
       return { ...state, content: newContent }
     }
-    case 'ADD_ROW': {
+    case ADD_ROW: {
       const newContent = [...state.content]
       const newUniqueKeys = [...state.uniqueKeys]
       const [header] = newContent
@@ -15,7 +22,7 @@ export default (state, action) => {
       newUniqueKeys.splice(action.rowIndex, 0, uuid.v4())
       return { ...state, content: newContent, uniqueKeys: newUniqueKeys }
     }
-    case 'REMOVE_ROW': {
+    case REMOVE_ROW: {
       const newContent = state.content.filter(
         (_, index) => index !== action.rowIndex
       )
@@ -24,7 +31,7 @@ export default (state, action) => {
       )
       return { ...state, content: newContent, uniqueKeys: newUniqueKeys }
     }
-    case 'ADD_COLUMN': {
+    case ADD_COLUMN: {
       const newContent = state.content.map(row => {
         const newRow = [...row]
         newRow.splice(action.colIndex - 1, 0, '')
@@ -32,7 +39,7 @@ export default (state, action) => {
       })
       return { ...state, content: newContent }
     }
-    case 'REMOVE_COLUMN': {
+    case REMOVE_COLUMN: {
       const newContent = state.content.map(row =>
         row.filter((_, index) => index !== action.colIndex - 1)
       )
